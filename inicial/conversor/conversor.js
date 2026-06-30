fa9a3d4e5ff827d85efcfc06cc8a62ee760f92c5
Rodrigo Gonçalves <rodrigodev.25@gmail.com>
2026-06-29 21:54:40 -0300

    Update conversor.js

--- inicial/conversor/conversor.js
+++ inicial/conversor/conversor.js
@@ -1,51 +1,31 @@
-const valorInput = document.getElementById("valor");
-const moedaOrigem = document.getElementById("moedaOrigem");
-const moedaDestino = document.getElementById("moedaDestino");
-const resultado = document.getElementById("resultado");
-valorInput.addEventListener("input", converter);
-moedaOrigem.addEventListener("change", converter);
-moedaDestino.addEventListener("change", converter);
 async function converter() {
-    const valor = valorInput.value;
+    const valor = parseFloat(valorInput.value);
+
     if (!valor || valor <= 0) {
         resultado.textContent = "Resultado";
         return;
     }
+
     if (moedaOrigem.value === moedaDestino.value) {
-        const valorFormatado = new Intl.NumberFormat("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        }).format(valor);
         resultado.textContent =
-            `${valorFormatado} ${moedaOrigem.value} = ${valorFormatado} ${moedaDestino.value}`;
+            `${valor.toFixed(2)} ${moedaOrigem.value} = ${valor.toFixed(2)} ${moedaDestino.value}`;
         return;
-        }
+    }
+
     try {
         const response = await fetch(
-    `https://open.er-api.com/v6/latest/${moedaOrigem.value}`
+            `https://open.er-api.com/v6/latest/${moedaOrigem.value}`
         );
 
         const data = await response.json();
 
         const taxa = data.rates[moedaDestino.value];
         const convertido = valor * taxa;
-        const valorFormatado = new Intl.NumberFormat("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        }).format(valor);
-        const convertidoFormatado = new Intl.NumberFormat("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        }).format(convertido);
+
         resultado.textContent =
-            `${valorFormatado} ${moedaOrigem.value} = ${convertidoFormatado} ${moedaDestino.value}`;
-    } catch {
+            `${valor.toFixed(2)} ${moedaOrigem.value} = ${convertido.toFixed(2)} ${moedaDestino.value}`;
+
+    } catch (erro) {
         resultado.textContent = "Erro ao buscar cotação.";
     }
-}
-function inverterMoedas() {
-    const temp = moedaOrigem.value;
-    moedaOrigem.value = moedaDestino.value;
-    moedaDestino.value = temp;
-    converter();
 }
